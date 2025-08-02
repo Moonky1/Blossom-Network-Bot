baeb3c3d054b262288532ff62c13a97b3b592c37
Moonky1 <simon.francoau@gmail.com>
2025-08-02 18:57:28 +1000

    add debug logs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log('▶ DISCORD_TOKEN raw:', process.env.DISCORD_TOKEN);
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
