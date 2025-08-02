c6e3ea7045a423e35cb65c569b4589a70c1fd3c9
Moonky1 <simon.francoau@gmail.com>
2025-08-02 13:01:10 +1000

    Add Blossom bot code and config for 24/7 deploy

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+  console.log(`¡Blossom listo! Conectado como ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN)
+  .then(() => console.log('Login OK'))
+  .catch(err => console.error('Login ERR:', err));
+
+// Si Railway lo detecta como Web Service, expón un puerto:
+require('http')
+  .createServer((req, res) => res.end('OK'))
+  .listen(process.env.PORT || 3000);
